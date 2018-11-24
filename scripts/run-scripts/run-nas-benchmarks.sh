88b605fab8834ed2f1f89cafd10171e6d473cec5
Aleksandr Maramzin <maramzinalexandr@gmail.com>
2018-11-24 00:29:54 +0000

    [!] Fixed metrics tool NAS benchmarks running script [+] Updated icc opt report parser external repository reference

--- scripts/run-scripts/run-nas-benchmarks.sh
+++ scripts/run-scripts/run-nas-benchmarks.sh
@@ -19,23 +19,18 @@ mkdir ${NAS_METRICS_REPORTS_DIR}
 cd ${NAS_PPAR_METRICS_RUN_DIR}
 
 make clean
-if [[ -e ${NAS_METRICS_REPORTS_DIR} ]]; then
-    rm -rf ${NAS_METRICS_REPORTS_DIR}
-fi
-mkdir ${NAS_METRICS_REPORTS_DIR}
-
 for BenchmarkFolderName in BT CG DC EP FT IS LU MG SP UA; do
     echo "Removing ${BenchmarkFolderName}/${BenchmarkFolderName}.metrics.excel"
     rm -rf ${BenchmarkFolderName}/${BenchmarkFolderName}.metrics.excel
 done
-echo "Removing ${METRICS_RUN_DIR}/REPORT.metrics.excel"
-rm -rf ${METRICS_RUN_DIR}/REPORT.metrics.excel
+echo "Removing ${NAS_PPAR_METRICS_RUN_DIR}/REPORT.metrics.excel"
+rm -rf ${NAS_PPAR_METRICS_RUN_DIR}/REPORT.metrics.excel
 
 for BenchmarkFolderName in BT CG DC EP FT IS LU MG SP UA; do
-    echo "Removing ${METRICS_RUN_DIR}/${BenchmarkFolderName}/*.metrics"
-    rm -rf ${METRICS_RUN_DIR}/${BenchmarkFolderName}/*.metrics
-    echo "Removing ${METRICS_RUN_DIR}/${BenchmarkFolderName}/metrics.excel"
-    rm -rf ${METRICS_RUN_DIR}/${BenchmarkFolderName}/metrics.excel
+    echo "Removing ${NAS_PPAR_METRICS_RUN_DIR}/${BenchmarkFolderName}/*.metrics"
+    rm -rf ${NAS_PPAR_METRICS_RUN_DIR}/${BenchmarkFolderName}/*.metrics
+    echo "Removing ${NAS_PPAR_METRICS_RUN_DIR}/${BenchmarkFolderName}/metrics.excel"
+    rm -rf ${NAS_PPAR_METRICS_RUN_DIR}/${BenchmarkFolderName}/metrics.excel
 done 
 
 echo "= Running PPar metrics collector on benchmarks ="
@@ -46,12 +41,13 @@ for BenchmarkName in bt cg dc ep ft is lu mg sp ua; do
 done 
 
 for BenchmarkFolderName in BT CG DC EP FT IS LU MG SP UA; do
-    echo "Benchmark: ${BenchmarkFolderName}" >> ${METRICS_REPORT_DIR}/${BenchmarkFolderName}.metrics.excel
-    cat ${METRICS_RUN_DIR}/${BenchmarkFolderName}/metrics.excel >> ${METRICS_REPORT_DIR}/${BenchmarkFolderName}.metrics.excel
-    echo "${BenchmarkFolderName} benchmark report: ${METRICS_REPORT_DIR}/${BenchmarkFolderName}.metrics.excel"
-    cat ${METRICS_REPORT_DIR}/${BenchmarkFolderName}.metrics.excel >> ${METRICS_REPORT_DIR}/REPORT.metrics.excel
+    echo "Benchmark: ${BenchmarkFolderName}" >> ${NAS_METRICS_REPORTS_DIR}/${BenchmarkFolderName}.metrics.excel
+    cat ${NAS_PPAR_METRICS_RUN_DIR}/${BenchmarkFolderName}/metrics.excel >> ${NAS_METRICS_REPORTS_DIR}/${BenchmarkFolderName}.metrics.excel
+    echo "${BenchmarkFolderName} benchmark report: ${NAS_METRICS_REPORTS_DIR}/${BenchmarkFolderName}.metrics.excel"
+    cat ${NAS_METRICS_REPORTS_DIR}/${BenchmarkFolderName}.metrics.excel >> ${NAS_METRICS_REPORTS_DIR}/REPORT.metrics.excel
 done
-echo "Final report: ${METRICS_REPORT_DIR}/REPORT.metrics.excel"
+echo "Final report: ${NAS_METRICS_REPORTS_DIR}/REPORT.metrics.excel"
 
-cd ${PPAR_PROJECT_DIR}
+)
+
 echo "=== PPar tool finished! ==="

04768718e8edd34ed97eb0e5ab7138a8d4deea4e
Aleksandr Maramzin <maramzinalexandr@gmail.com>
2018-11-26 00:56:15 +0000

    [!] Fixed grep #pragma omp out of NAS benshmarks script [+] Added new ICC vs TRUE parallelisibility predictor script

--- scripts/analysis/icc-vs-true.py
+++ scripts/analysis/icc-vs-true.py
@@ -54,8 +54,9 @@ if __name__ == "__main__":
     loop_locations = data['loop-location']
     # prepare statistical learning labels 
     loop_icc_classifications = data['icc-parallel']
+    loop_true_classifications = data['true-parallel']
     # prepare statistical learning features 
-    features = data.drop(['loop-location','icc-parallel'], axis=1)
+    features = data.drop(['loop-location','icc-parallel','true-parallel'], axis=1)
 
     # remove outliers from the data
     if std_num != 0:
@@ -69,10 +70,12 @@ if __name__ == "__main__":
             filtered_idx &= filtered_idxs[metric]
 
         loop_icc_classifications = loop_icc_classifications[filtered_idx]
+        loop_true_classifications = loop_true_classifications[filtered_idx]
         idxs_to_drop = features.index.drop(filtered_idx)
         features = features.drop(idxs_to_drop)
 
         loop_icc_classifications = loop_icc_classifications.reset_index(drop=True)
+        loop_true_classifications = loop_true_classifications.reset_index(drop=True)
         features = features.reset_index(drop=True)
 
     # normalize the data
@@ -101,75 +104,6 @@ if __name__ == "__main__":
     # print the header into the report file
     print('DT.report', file=report_file)
 
-    # SVM for single metrics
-    for metric in ppar.metric_list:
-        dataset = metric_data[metric]
-        print("DT for single " + metric + " metric")
-
-        report_file.write("[ ") 
-        report_file.write(str(metric))
-        report_file.write(" ]\n") 
-
-        random_state = 12883823
-        for splits_num in [5,10,15,20,30]:
-            report_file.write("splits-num:" + str(splits_num)+ "\n")
-
-            rkf = RepeatedKFold(n_splits=splits_num, n_repeats=5, random_state=random_state)
-            
-            accuracy_samples = []
-            accuracy_rates = []
-            error_rates = []
-            safe_mispredict_rates = []
-            unsafe_mispredict_rates = []
-            for train, test in rkf.split(dataset):
-                training_data = dataset.iloc[train]
-                testing_data = dataset.iloc[test]
-                training_labels = loop_icc_classifications.iloc[train]
-                testing_labels = loop_icc_classifications.iloc[test]
-
-                # fit SVM model to the training dataset
-                clf = tree.DecisionTreeClassifier()
-                clf.fit(training_data.values.reshape(-1,1), training_labels)
-                # calculate prediction accuracy
-
-                predictions = clf.predict(testing_data.values.reshape(-1,1))
-                testing_labels = testing_labels.reset_index(drop=True)
-
-                mispredicts = 0
-                safe_mispredicts = 0
-                unsafe_mispredicts = 0
-                for i in range(0,len(predictions)):
-                    pred = predictions[i]
-                    test = testing_labels[i]
-                    if pred != test:
-                        # misprediction
-                        mispredicts += 1
-                        if pred == 1:
-                            unsafe_mispredicts += 1
-                        else:
-                            safe_mispredicts += 1
-
-                rate = 100 - mispredicts/len(predictions)
-                accuracy_rates.append(rate)
-
-                rate = mispredicts/len(predictions)
-                error_rates.append(rate)
-
-                rate = safe_mispredicts/mispredicts
-                safe_mispredict_rates.append(rate)
-
-                rate = unsafe_mispredicts/mispredicts
-                unsafe_mispredict_rates.append(rate)
-
-                accuracy = accuracy_score(testing_labels, clf.predict(testing_data.values.reshape(-1,1)))
-                accuracy_samples.append(accuracy)
-            report_file.write("dt-accuracy:" + "{0:.2f}".format(pd.Series(accuracy_samples).mean()*100) + "\n")
-            report_file.write("accuracy:" + "{0:.2f}".format(pd.Series(accuracy_rates).mean()*100) + "\n")
-            report_file.write("error:" + "{0:.2f}".format(pd.Series(error_rates).mean()*100) + "\n")
-            report_file.write("safe-mispredicts-rate:" + "{0:.2f}".format(pd.Series(safe_mispredict_rates).mean()*100) + "\n")
-            report_file.write("unsafe-mispredicts-rate:" + "{0:.2f}".format(pd.Series(unsafe_mispredict_rates).mean()*100) + "\n")
-            report_file.write("\n")
-
     # SVM for sets of metrics
     for metric_set in ppar.metric_sets:
         dataset = metric_set_data[metric_set]
@@ -188,11 +122,18 @@ if __name__ == "__main__":
             rkf = RepeatedKFold(n_splits=splits_num, n_repeats=5, random_state=random_state)
 
             accuracy_samples = []
+            hit_rate = []
+            miss_rate = []
+            win_rate = []
+            fail_rate = []
+            safe_fail_rate = []
+            unsafe_fail_rate = []
             for train, test in rkf.split(dataset):
                 training_data = dataset.iloc[train]
                 testing_data = dataset.iloc[test]
-                training_labels = loop_icc_classifications.iloc[train]
+                training_labels = loop_true_classifications.iloc[train]
                 testing_labels = loop_icc_classifications.iloc[test]
+                true_testing_labels = loop_true_classifications.iloc[test]
 
                 # fit SVM model to the training dataset
                 clf = tree.DecisionTreeClassifier()
@@ -200,41 +141,61 @@ if __name__ == "__main__":
 
                 predictions = clf.predict(testing_data)
                 testing_labels = testing_labels.reset_index(drop=True)
+                true_testing_labels = true_testing_labels.reset_index(drop=True)
 
-                mispredicts = 0
-                safe_mispredicts = 0
-                unsafe_mispredicts = 0
+                hits = 0
+                misses = 0
+                icc_wrong = 0
+                icc_correct = 0
+                safe = 0
+                unsafe = 0
                 for i in range(0,len(predictions)):
                     pred = predictions[i]
-                    test = testing_labels[i]
-                    if pred != test:
-                        # misprediction
-                        mispredicts += 1
-                        if pred == 1:
-                            unsafe_mispredicts += 1
+                    icc = testing_labels[i]
+                    true = true_testing_labels[i]
+                    
+                    if pred == icc:
+                        hits += 1
+                    else:
+                        misses += 1
+                        if pred == true:
+                            icc_wrong += 1
                         else:
-                            safe_mispredicts += 1
+                            icc_correct += 1
+                            if pred == True:
+                                unsafe += 1
+                            else:
+                                safe += 1
 
-                rate = 100 - mispredicts/len(predictions)
-                accuracy_rates.append(rate)
+                rate = hits/len(predictions)
+                hit_rate.append(rate)
 
-                rate = mispredicts/len(predictions)
-                error_rates.append(rate)
+                rate = misses/len(predictions)
+                miss_rate.append(rate)
 
-                rate = safe_mispredicts/mispredicts
-                safe_mispredict_rates.append(rate)
+                rate = icc_wrong/len(predictions)
+                win_rate.append(rate)
 
-                rate = unsafe_mispredicts/mispredicts
-                unsafe_mispredict_rates.append(rate)
+                rate = icc_correct/len(predictions)
+                fail_rate.append(rate)
+
+                rate = safe/len(predictions)
+                safe_fail_rate.append(rate)
+
+                rate = unsafe/len(predictions)
+                unsafe_fail_rate.append(rate)
 
                 # calculate prediction accuracy 
                 accuracy = accuracy_score(testing_labels, clf.predict(testing_data))
                 accuracy_samples.append(accuracy)
             report_file.write("dt-accuracy:" + "{0:.2f}".format(pd.Series(accuracy_samples).mean()*100) + "\n")
-            report_file.write("accuracy:" + "{0:.2f}".format(pd.Series(accuracy_rates).mean()*100) + "\n")
-            report_file.write("error:" + "{0:.2f}".format(pd.Series(error_rates).mean()*100) + "\n")
-            report_file.write("safe-mispredicts-rate:" + "{0:.2f}".format(pd.Series(safe_mispredict_rates).mean()*100) + "\n")
-            report_file.write("unsafe-mispredicts-rate:" + "{0:.2f}".format(pd.Series(unsafe_mispredict_rates).mean()*100) + "\n")
+            report_file.write("hits:" + "{0:.2f}".format(pd.Series(hit_rate).mean()*100) + "\n")
+            report_file.write("misses:" + "{0:.2f}".format(pd.Series(miss_rate).mean()*100) + "\n")
+            report_file.write("wins:" + "{0:.2f}".format(pd.Series(win_rate).mean()*100) + "\n")
+            report_file.write("fails:" + "{0:.2f}".format(pd.Series(fail_rate).mean()*100) + "\n")
+            report_file.write("safe-fails:" + "{0:.2f}".format(pd.Series(safe_fail_rate).mean()*100) + "\n")
+            report_file.write("unsafe-fails:" + "{0:.2f}".format(pd.Series(unsafe_fail_rate).mean()*100) + "\n")
+
             report_file.write("\n")
    
     report_file.close()
--- scripts/grep-pragma-openmp-loops.sh
+++ scripts/grep-pragma-openmp-loops.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-PPAR_PROJECT_DIR="${PWD}"
-BENCHMARKS_DIR="${PPAR_PROJECT_DIR}/benchmarks/"
-NAS_SOURCES_DIR="${BENCHMARKS_DIR}/nauseous-omp/"
-NAS_OMP_SOURCES_DIR="${NAS_SOURCES_DIR}"
+source $(dirname "$0")/include/project-environment.sh
 
-REPORT_DIR="${PPAR_PROJECT_DIR}/reports/ppar-metrics/"
-GREP_OMP_REPORT_DIR=${REPORT_DIR}/grep-omp-report/
+echo "< grep-pragma-openmp-loops.sh script >"
+echo "=> Grepping #pragma omp out of NAS benchmark sources"
 
-if [[ -e ${GREP_OMP_REPORT_DIR} ]]; then
-    rm -rf ${GREP_OMP_REPORT_DIR}
+if [[ -e ${NAS_OMP_GREP_REPORTS_DIR} ]]; then
+    rm -rf ${NAS_OMP_GREP_REPORTS_DIR}
 fi
-mkdir ${GREP_OMP_REPORT_DIR}
+mkdir ${NAS_OMP_GREP_REPORTS_DIR}
 
 # extract all C/C++ loop related OpenMP pragmas out of source code files
-grep "#pragma omp parallel for" ${NAS_OMP_SOURCES_DIR} -R -n > ${GREP_OMP_REPORT_DIR}/REPORT.omp.raw
-grep "#pragma omp for" ${NAS_SOURCES_DIR} -R -n >> ${GREP_OMP_REPORT_DIR}/REPORT.omp.raw
+grep "#pragma omp parallel for" ${NAS_OMP_SOURCES_DIR} -R -n > ${NAS_OMP_GREP_REPORTS_DIR}/REPORT.omp.raw
+grep "#pragma omp for" ${NAS_OMP_SOURCES_DIR} -R -n >> ${NAS_OMP_GREP_REPORTS_DIR}/REPORT.omp.raw
 
-cat ${GREP_OMP_REPORT_DIR}/REPORT.omp.raw | 
+cat ${NAS_OMP_GREP_REPORTS_DIR}/REPORT.omp.raw | 
 sed -r 's/\/\//\//g' |
 sed -r 's/(.+):(.+):(.+)\\/\1(\2):?/' |
 sed -r 's/(.+):([0-9]+):([^?]+)/echo "\1($((\2+1))):1"/ge' \
-    > ${GREP_OMP_REPORT_DIR}/REPORT.omp
+    > ${NAS_OMP_GREP_REPORTS_DIR}/REPORT.omp

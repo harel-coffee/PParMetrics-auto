ed12e1e92ca0fe6027dcc5f63a1b2d9a3a5170f5
Aleksandr Maramzin <maramzinalexandr@gmail.com>
2018-12-19 01:13:11 +0000

    [+] Added new examples to the /playground

---
+++ playground/src/loop-nest-indep.cpp
@@ -0,0 +1,30 @@
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+
+const int size_i = 100;
+const int size_j = 10;
+const int size_k = 5;
+
+int main() {
+    int a[size_i][size_j][size_k];
+ 
+    for (int i = 0; i < size_i; i++) {
+        for (int j = 0; j < size_j; j++) {
+            for (int k = 0; k < size_k; k++) {
+                a[i][j][k] = std::rand() % 10;
+            }
+        }
+    }
+
+    // parallelisible
+    for (int i = 0; i < size_i; i++) {
+        for (int j = i; j < size_j; j++) {
+            for (int k = 1; k < size_k; k++) {
+                a[i][j][k] = a[i][j][k]+1; 
+            }
+        }
+    }
+
+    return 0;
+}

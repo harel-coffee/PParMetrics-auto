a5ac96ad23e58c058f598315eb1e27a984f48f85
Aleksandr Maramzin <maramzinalexandr@gmail.com>
2019-10-28 11:54:33 +0000

    [+] Added a shared library object with all LLVM passes linked in.

---
+++ build/CMakeFiles/ppar.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ppar.dir/src/DecoupleLoopsPass.cpp.o"
+  "CMakeFiles/ppar.dir/src/DotPrinter.cpp.o"
+  "CMakeFiles/ppar.dir/src/FunctionLoopInfo.cpp.o"
+  "CMakeFiles/ppar.dir/src/GraphPasses.cpp.o"
+  "CMakeFiles/ppar.dir/src/GraphPrinters.cpp.o"
+  "CMakeFiles/ppar.dir/src/MetricPrinter.cpp.o"
+  "CMakeFiles/ppar.dir/src/MetricsPasses.cpp.o"
+  "libppar.pdb"
+  "libppar.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ppar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
